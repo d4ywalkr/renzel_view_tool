8854c00896760e8d22d19cf9c61a46120b7cbe8c
Renzo <renzoscheffer@gmail.com>
2018-06-04 15:34:02 +0200

    renzel tool initial commit

---
+++ lib/renzel_view_tool.rb
@@ -0,0 +1,6 @@
+require "renzel_view_tool/version"
+require "renzel_view_tool/renderer"
+
+module RenzelViewTool
+  # Your code goes here...
+end
---
+++ lib/renzel_view_tool/version.rb
@@ -0,0 +1,3 @@
+module RenzelViewTool
+  VERSION = "0.1.0"
+end
